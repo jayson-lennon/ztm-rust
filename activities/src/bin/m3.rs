b8d1e7f17637e321c739be62d18f6a8e7c3a5f31
Jayson Lennon <jayson@jaysonlennon.dev>
2021-09-10 18:42:00 -0700

    Add m3 activity

---
+++ activities/src/bin/m3.rs
@@ -0,0 +1,8 @@
+// Topic: Basic macro repetitions
+//
+// Requirements:
+//   * Create a macro to generate hashmaps.
+//   * The macro must be able to accept multiple key/value pairs.
+//   * Print out the generated hashmap using the `dbg!` macro to ensure it works.
+
+fn main() {}
