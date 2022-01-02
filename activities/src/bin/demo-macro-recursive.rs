ed23dc04bd9eba846444c949dd458dd74cbb7f69
Jayson Lennon <jayson@jaysonlennon.dev>
2022-01-02 14:45:05 -0800

    Add supplemental code

--- activities/src/bin/demo-macro-recursive.rs
+++ activities/src/bin/demo-macro-recursive.rs
@@ -1,1 +1,17 @@
-fn main() {}
+macro_rules! html {
+}
+
+#[allow(unused_must_use)]
+fn main() {
+    use std::fmt::Write;
+    let mut data = String::new();
+    html!(&mut data,
+    html[
+        head[ title["Demo title"] ]
+        body[
+            h1["Sample"]
+            p["This is a macro demo"]
+        ]
+    ]);
+    dbg!(data);
+}
