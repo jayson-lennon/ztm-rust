d0e88077eac47fc3259434401b1b29488aeedcff
Jayson Lennon <jayson@jaysonlennon.dev>
2022-08-09 19:32:13 -0700

    Change a18

--- activities/src/bin/a18.rs
+++ activities/src/bin/a18.rs
@@ -1,14 +1,19 @@
 // Topic: Result
 //
 // Requirements:
-// * Determine if a customer is able to make a restricted purchase
-// * Restricted purchases require that the age of the customer
-//   is at least 21
-//
-// Notes:
-// * Use a struct to store at least the age of a customer
-// * Use a function to determine if a customer can make a restricted purchase
-// * Return a result from the function
-// * The Err variant should detail the reason why they cannot make a purchase
+// * Create an structure named `Adult` that represents a person aged 21 or older:
+//   * The structure must contain the person's name and age
+//   * Implement Debug print functionality using `derive`
+// * Implement a `new` function for the `Adult` structure that returns a Result:
+//   * The Ok variant should contain the initialized structure, but only
+//     if the person is aged 21 or older
+//   * The Err variant should contain a String (or &str) that explains why
+//     the structure could not be created
+// * Instantiate two `Adult` structures:
+//   * One should be aged under 21
+//   * One should be 21 or over
+// * Use `match` to print out a message for each `Adult`:
+//   * For the Ok variant, print any message you want
+//   * For the Err variant, print out the error message
 
 fn main() {}
