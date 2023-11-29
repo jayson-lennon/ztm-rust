8f7509b610042f7bfdfd137cff20b82a55d67f72
Jayson Lennon <jayson@jaysonlennon.dev>
2023-11-29 14:27:34 -0800

    Remove requirement

--- activities/src/bin/a36.rs
+++ activities/src/bin/a36.rs
@@ -4,7 +4,6 @@
 // * Print pairs of numbers and their sums as they are streamed from a data source
 // * If only one number is received, then print "Unpaired value: V",
 //   where V is the value
-// * If no numbers are received, print "Data stream complete"
 //
 // Notes:
 // * A simulated data stream is already configured in the code
