ef09158607457118e30a354f70072e0f4539f590
Jayson Lennon <jayson@jaysonlennon.dev>
2023-11-29 14:26:05 -0800

    Fix activity

--- activities/src/bin/a36.rs
+++ activities/src/bin/a36.rs
@@ -4,7 +4,6 @@
 // * Print pairs of numbers and their sums as they are streamed from a data source
 // * If only one number is received, then print "Unpaired value: V",
 //   where V is the value
-// * If no numbers are received, print "Data stream complete"
 //
 // Notes:
 // * A simulated data stream is already configured in the code
@@ -17,15 +16,14 @@ fn data() -> &'static [u64] {
 fn process_chunk(data: &[u64]) {
     match data {
         [lhs, rhs] => println!("{}+{}={}", lhs, rhs, lhs + rhs),
-        [single] => println!("Unpaired value: single"),
-        [] => println!("Data stream complete"),
+        [single] => println!("Unpaired value: {single}"),
         [..] => unreachable!("chunk size should be at most 2"),
     }
 }
 
 fn main() {
     // `stream` is an iterator of Option<&[u64]>
-    let mut stream = data().chunks(2);
+    let stream = data().chunks(2);
     for chunk in stream {
         process_chunk(chunk);
     }
