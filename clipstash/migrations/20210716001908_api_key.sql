85a1c06663b2cc9aa3011dc3f9a82a91411f966c
Jayson Lennon <jayson@jaysonlennon.dev>
2024-08-11 21:50:57 -0700

    Remove clipstash

--- clipstash/migrations/20210716001908_api_key.sql
+++
@@ -1,5 +0,0 @@
--- Add migration script here
-CREATE TABLE IF NOT EXISTS api_keys
-(
-    api_key BLOB PRIMARY KEY
-);
--- clipstash/src/lib/domain/mod.rs
+++
@@ -1,7 +0,0 @@
-//! Business-specific modules and functions
-
-pub mod clip;
-pub mod time;
-pub mod maintenance;
-
-pub use clip::Clip;
