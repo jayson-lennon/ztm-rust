c9e2422aace217774ecea3983f3adb56644ebcf3
Jayson Lennon <jayson@jaysonlennon.dev>
2024-08-17 09:23:27 -0700

    update comment

--- activities/src/bin/mc-03.rs
+++ activities/src/bin/mc-03.rs
@@ -7,16 +7,20 @@
 //
 // Requirements:
 // - Create an extension trait named `AccountExt` that adds two methods to the `Account` trait:
-//   - `withdraw`: withdraws a specified amount from the account.
-//   - `refund`: refunds a specified amount to the account.
+//   - `withdraw`: removes a specified amount from the account.
+//   - `deposit`: adds a specified amount to the account.
 // - Implement the `AccountExtensions` trait for any type that implements the `Account` trait by
 //   using a blanket implementation.
+// - Do not change any of the existing code. Only add and implement an extension trait.
 //
 // Expected Output:
 //   Processed transaction of $50.00. New balance: $150.00
 //   Processed transaction of -$30.00. New balance: $120.00
 //   Processed transaction of $20.00. New balance: $140.00
 
+/**********************************************
+* Do not change
+**********************************************/
 trait Account {
     fn process(&mut self, amount: f64);
 }
@@ -25,6 +29,9 @@ struct BankAccount {
     balance: f64,
 }
 
+/**********************************************
+* Do not change
+**********************************************/
 impl BankAccount {
     fn new(initial_balance: f64) -> Self {
         BankAccount {
@@ -33,6 +40,9 @@ impl BankAccount {
     }
 }
 
+/**********************************************
+* Do not change
+**********************************************/
 impl Account for BankAccount {
     fn process(&mut self, amount: f64) {
         self.balance += amount;
@@ -66,6 +76,9 @@ where
     }
 }
 
+/**********************************************
+* Do not change
+**********************************************/
 fn main() {
     let mut account = BankAccount::new(100.0);
 
