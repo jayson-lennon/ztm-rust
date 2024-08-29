b331397ec4aaffb0a94f007f2116e57b8459858c
Jayson Lennon <jayson@jaysonlennon.dev>
2024-08-29 10:15:07 -0700

    Add new abstract network solution

--- activities/src/bin/demo-abstract-network-request.rs
+++ activities/src/bin/demo-abstract-network-request.rs
@@ -17,20 +17,80 @@ struct PersonResponse {
     data: Vec<Person>,
 }
 
-fn get_person(client: &reqwest::blocking::Client) -> Result<PersonResponse> {
-    let response: PersonResponse = client
-        .get("https://fakerapi.it/api/v1/custom?_quantity=1&name=name&country=country")
-        .send()?
-        .json()?;
+trait DataFetcher {
+    fn get_person(&self) -> Result<PersonResponse>;
+}
 
-    Ok(response)
+#[derive(Debug, Clone, Default)]
+struct ReqwestFetcher {
+    client: reqwest::blocking::Client,
+}
+
+impl DataFetcher for ReqwestFetcher {
+    fn get_person(&self) -> Result<PersonResponse> {
+        let response: PersonResponse = self
+            .client
+            .get("https://fakerapi.it/api/v1/custom?_quantity=1&name=name&country=country")
+            .send()?
+            .json()?;
+
+        Ok(response)
+    }
+}
+
+struct App {
+    fetcher: Box<dyn DataFetcher>,
+}
+
+impl Default for App {
+    fn default() -> Self {
+        Self {
+            fetcher: Box::new(ReqwestFetcher::default()),
+        }
+    }
+}
+
+impl App {
+    pub fn fetch_person(&self) -> Result<PersonResponse> {
+        self.fetcher.get_person()
+    }
 }
 
 fn main() -> Result<()> {
-    let client = reqwest::blocking::Client::default();
-    let person = get_person(&client);
+    let app = App::default();
+    let person = app.fetch_person()?;
 
     println!("{person:?}");
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct TestFetcher;
+
+    impl DataFetcher for TestFetcher {
+        fn get_person(&self) -> Result<PersonResponse> {
+            Ok(PersonResponse {
+                status: "OK".to_string(),
+                code: 200,
+                total: 1,
+                data: vec![Person {
+                    name: "test name".to_string(),
+                    country: "test country".to_string(),
+                }],
+            })
+        }
+    }
+
+    #[test]
+    fn fetches_data() {
+        let app = App {
+            fetcher: Box::new(TestFetcher),
+        };
+        let person = app.fetch_person();
+        assert!(person.is_ok());
+    }
 }
