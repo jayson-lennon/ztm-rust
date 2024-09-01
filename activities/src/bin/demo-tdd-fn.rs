63aaf9342fc4c0286192da315698dceb624d681e
Jayson Lennon <jayson@jaysonlennon.dev>
2024-09-01 10:51:18 -0700

    new solution

--- activities/src/bin/demo-tdd-fn.rs
+++ activities/src/bin/demo-tdd-fn.rs
@@ -1,21 +1,23 @@
 fn main() {}
 
-fn add(lhs: u32, rhs: u32) -> u32 {
+fn add(lhs: i32, rhs: i32) -> i32 {
     lhs + rhs
 }
 
-fn sub(lhs: i32, rhs: i32) -> i32 {
-    lhs - rhs
+fn max(lhs: i32, rhs: i32) -> Option<i32> {
+    if lhs > rhs {
+        Some(lhs)
+    } else if lhs < rhs {
+        Some(rhs)
+    } else {
+        None
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use pretty_assertions::assert_eq;
 
-    // test name describes:
-    // 1. what we are doing
-    // 2. what we are expecting
     #[test]
     fn add_function_adds_two_numbers() {
         let result = add(2, 3);
@@ -23,14 +25,26 @@ mod tests {
     }
 
     #[test]
-    fn subtract_function_subtracts_two_numbers() {
-        let result = sub(10, 2);
-        assert_eq!(result, 8);
+    fn add_function_adds_two_negative_numbers() {
+        let result = add(-1, 4);
+        assert_eq!(result, 3);
     }
 
     #[test]
-    fn subtract_function_produces_a_negative_result_when_subtracting_a_large_number() {
-        let result = sub(2, 10);
-        assert_eq!(result, -8);
+    fn max_function_returns_first_number_when_it_is_greatest() {
+        let result = max(5, 1);
+        assert_eq!(result, Some(5));
+    }
+
+    #[test]
+    fn max_function_returns_second_number_when_it_is_greatest() {
+        let result = max(2, 3);
+        assert_eq!(result, Some(3));
+    }
+
+    #[test]
+    fn max_function_returns_none_when_both_numbers_are_equal() {
+        let result = max(4, 4);
+        assert!(result.is_none());
     }
 }
