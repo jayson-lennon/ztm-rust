11e8b4b0bc7000c678604af82bce527cd5b0e261
Jayson Lennon <jayson@jaysonlennon.dev>
2024-09-08 12:42:03 -0700

    .

--- projects/track/src/feature.rs
+++
@@ -1,8 +0,0 @@
-<<<<<<< Updated upstream
-pub mod backend;
-pub mod report_fmt;
-=======
-pub mod cli;
-pub mod report_fmt;
-pub mod tracker;
->>>>>>> Stashed changes
--- projects/track/src/lib.rs
+++
@@ -1,6 +0,0 @@
-#![allow(clippy::default_constructed_unit_structs)]
-
-pub mod common;
-pub mod error;
-pub mod feature;
-pub mod init;
