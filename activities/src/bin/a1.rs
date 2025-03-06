839e8096beaeb62dbbf290d9d92b2d1c3f673a39
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2025-03-06 13:17:36 -0600

    completed activity a1

--- activities/src/bin/a1.rs
+++ activities/src/bin/a1.rs
@@ -8,5 +8,16 @@
 // * Use a function to display your last name
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+fn first_name() {
+    println!("Tommy");
+}
+
+fn last_name() {
+    println!("Oliver");
+}
+
+fn main() {
+    first_name();
+    last_name();
+}
 
